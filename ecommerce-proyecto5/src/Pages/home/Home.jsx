8a4e9609523b04b248f1d9ae883620ce0cce5deb
Marta Nicoli <marta.nicoli@gmail.com>
2023-04-02 21:22:48 -0400

    Actualización

--- ecommerce-proyecto5/src/Pages/home/Home.jsx
+++ ecommerce-proyecto5/src/Pages/home/Home.jsx
@@ -4,27 +4,29 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Carousel from 'react-bootstrap/Carousel';
 import React from 'react';
+import './Home.css';
 
 
 const Home = () => {
-  return(
+  return (
     <div>
+      <ContenedorPublicidad />
       <CarrucelHome />
       <LoMasVendido />
 
-     
+
     </div>
   )
 }
 const LoMasVendido = () => {
   return (
-    <div>   
+    <div>
       <h1 style={{ fontSize: '32px', textAlign: 'center' }}>Lo Más Vendido</h1>
       <Row>
         <Col >
           <Card >
-          <Card.Header>Featured</Card.Header>
-            <Card.Img variant="top" src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8TW9kYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="S"  style={{width : '200px',height: '250px'}} />
+            <Card.Header>Featured</Card.Header>
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8TW9kYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="S" style={{ width: '200px', height: '250px' }} />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
@@ -37,8 +39,8 @@ const LoMasVendido = () => {
         </Col>
         <Col>
           <Card>
-          <Card.Header>Featured</Card.Header>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1571678432797-0fd4f9061c4c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTEyfHxNb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="S"  style={{width : '200px',height: '250px'}} />
+            <Card.Header>Featured</Card.Header>
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1571678432797-0fd4f9061c4c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTEyfHxNb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="S" style={{ width: '200px', height: '250px' }} />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
@@ -51,8 +53,8 @@ const LoMasVendido = () => {
         </Col>
         <Col>
           <Card>
-          <Card.Header>Featured</Card.Header>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1596902852858-126144389ee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODB8fE1vZGF8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" alt="S"  style={{width : '200px',height: '250px'}} />
+            <Card.Header>Featured</Card.Header>
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1596902852858-126144389ee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODB8fE1vZGF8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" alt="S" style={{ width: '200px', height: '250px' }} />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
@@ -65,8 +67,8 @@ const LoMasVendido = () => {
         </Col>
         <Col>
           <Card>
-          <Card.Header>Featured</Card.Header>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1513094735237-8f2714d57c13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjAwfHxNb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="S"  style={{width : '200px',height: '250px'}} />
+            <Card.Header>Featured</Card.Header>
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1513094735237-8f2714d57c13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjAwfHxNb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="S" style={{ width: '200px', height: '250px' }} />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
@@ -77,22 +79,30 @@ const LoMasVendido = () => {
             </Card.Body>
           </Card>
         </Col>
-    </Row>
+      </Row>
 
-  
+
     </div>
 
-    
+
   )
 }
 
 
+const ContenedorPublicidad = () => (
+  <div className="publicidad">
+    <h1>Contenedor de publicidad</h1>
+  </div>
+);
+
+
+
 const CarrucelHome = () => {
   return (
-<>
-  <Carousel>
-      <Carousel.Item>
-          <img
+    <>
+      <Carousel style={{ textAlign: 'center', paddingTop: '10px' }}>
+        <Carousel.Item>
+        <img
             className="d-linea"
             src="https://images.unsplash.com/photo-1562572159-4efc207f5aff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjEwfHxNb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
             alt="First slide"
@@ -110,39 +120,70 @@ const CarrucelHome = () => {
             alt="Third slide"
             style={{width : '400px',height: '400px'}}
           />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
+
+          <Carousel.Caption>
+            <h3>Moda Mujer</h3>
+            <p>Ropa y accesorios diseñados para la exigente mujer moderna.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+        
+<img
+className="d-linea"
+src="https://plus.unsplash.com/premium_photo-1675129933526-5eb24a763248?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU1fHxtb2RhJTIwZGUlMjBob21icmV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+alt="First slide"
+style={{width : '400px',height: '400px'}} 
+/>
       <img
-            className="d-linea"
-            src="https://plus.unsplash.com/premium_photo-1675129933526-5eb24a763248?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU1fHxtb2RhJTIwZGUlMjBob21icmV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-            alt="First slide"
-            style={{width : '400px',height: '400px'}} 
-          />
-                  <img
-            className="d-linea"
-            src="https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjczfHxtb2RhJTIwZGUlMjBob21icmV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-            alt="Second slide"
-            style={{width : '400px',height: '400px'}} 
-          />
+className="d-linea"
+src="https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjczfHxtb2RhJTIwZGUlMjBob21icmV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+alt="Second slide"
+style={{width : '400px',height: '400px'}} 
+/>
+<img
+className="d-linea"
+src="https://images.unsplash.com/photo-1581381685617-4dc270458aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzYxfHxtb2RhJTIwZGUlMjBob21icmV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+alt="Third slide"
+style={{width : '400px',height: '400px'}}
+/>
+
+          <Carousel.Caption>
+            <h3>Moda Hombre</h3>
+            <p>Moda masculina desde lo clásico y elegante hasta lo moderno y casual.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+
+        <Carousel.Item>
+
           <img
-            className="d-linea"
-            src="https://images.unsplash.com/photo-1581381685617-4dc270458aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzYxfHxtb2RhJTIwZGUlMjBob21icmV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+            className="Carousel"
+            src="https://images.unsplash.com/photo-1544413164-5f1b361f5b69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
             alt="Third slide"
-            style={{width : '400px',height: '400px'}}
+            style={{ width: '400px', height: '400px' }}
           />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+          <img
+            className="Carousel"
+            src="https://images.unsplash.com/photo-1622288664652-96211d818e8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            alt="Third slide"
+            style={{ width: '400px', height: '400px' }}
+          />
 
-    </Carousel>
-</>
+<img
+            className="Carousel"
+            src="https://plus.unsplash.com/premium_photo-1661694212908-651b8c29fb1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="Third slide"
+            style={{ width: '400px', height: '400px' }}
+          />
+          <Carousel.Caption>
+            <h3>Moda Infantil</h3>
+            <p>Ropa y accesorios diseñados específicamente para niños y niñas de diferentes edades.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
   )
 
 }
